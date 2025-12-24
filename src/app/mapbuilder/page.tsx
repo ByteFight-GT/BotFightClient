12b2645c15786938e877ba48006075a5fb0c964b
crystaltine <donaldcenaaa@outlook.com>
2025-12-23 16:20:17 -0800

    create base nextjs directory structure for new pages (refactor inc)

---
+++ src/app/mapbuilder/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react';
+
+type MatchPlayerPageProps = {
+	
+};
+
+const MapBuilderPage = (props: MatchPlayerPageProps) => {
+	return (
+		<div>
+			<h1>Map Builder</h1>
+		</div>
+	);
+};
+
+export default MapBuilderPage;
---
+++ src/app/player/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from 'react';
+
+type MatchPlayerPageProps = {
+	
+};
+
+const MatchPlayerPage = (props: MatchPlayerPageProps) => {
+	return (
+		<div>
+			<h1>Match Player</h1>
+		</div>
+	);
+};
+
+export default MatchPlayerPage;
